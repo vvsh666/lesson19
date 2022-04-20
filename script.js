fee3e1d24f0598433fd7ddf8ff1f0328dfbd52b5
svv <vvsh1978@mail.ru>
2022-04-20 17:04:03 +0500

    feat: make lesson19-hard-2

--- script.js
+++ script.js
@@ -1,17 +1,51 @@
 const circle = document.querySelector('.circle')
 const wrapper = document.querySelector('.wrapper')
+const pause = document.getElementById('pause')
+const reset = document.getElementById('reset')
 let idAnimation
+let count = 20
+let check = true
 
 
 const changeCircle = () => {
-    if (circle.clientWidth < wrapper.clientWidth && circle.clientHeight < wrapper.clientHeight) {
-        circle.style.width = circle.clientWidth + 1 + 'px'
-        circle.style.height = circle.clientHeight + 1 + 'px'
-        console.log(circle.clientWidth);
+    if (count < 300) {
+        circle.style.width = count + 'px'
+        circle.style.height = count + 'px'
+        count += 1
+    } else if (count < 580) {
+        circle.style.width = 600 - count + 'px'
+        circle.style.height = 600 - count + 'px'
+        count += 1
+    } else if (count = 580) {
+        count = 20
     }
-    console.log(circle.clientWidth);
     idAnimation = requestAnimationFrame(changeCircle)
+
+
 }
+
+pause.addEventListener('click', function () {
+    if (check) {
+        cancelAnimationFrame(idAnimation)
+        this.textContent = 'Start'
+        check = false
+
+    } else {
+        idAnimation = requestAnimationFrame(changeCircle)
+        this.textContent = 'Pause'
+        check = true
+    }
+
+})
+
+reset.addEventListener('click', function () {
+    cancelAnimationFrame(idAnimation)
+    check = false
+    count = 20
+    pause.textContent = 'Start'
+    circle.style.width = ''
+    circle.style.height = ''
+})
 
 changeCircle()
 
